2f143d00d2c9ea646e207acdd344873a70e6de74
Àngel Fernández <angel.fernandez@holaluz.com>
2020-12-03 10:49:49 +0100

    Finish exercise 5 (day3 exercise 1)

--- test_day3/test_ex5.py
+++ test_day3/test_ex5.py
@@ -1,11 +1,20 @@
 from pathlib import Path
 from typing import List
 from collections import UserList
+from itertools import count
 
 
 class TreeRow(UserList):
     def __getitem__(self, key: int) -> str:
         return self.data[key % len(self.data)]
+
+
+TreeMap = list[TreeRow]
+
+
+def slope_tree_count(treemap: TreeMap) -> int:
+    col = count(3, 3)
+    return sum(1 for treerow in treemap[1:] if treerow[next(col)] == "#")
 
 
 def test_treerow_existing_positions():
@@ -20,7 +29,29 @@ def test_treerow_far_positions():
     assert treerow[13] == "#"
 
 
-# if __name__ == "__main__":
-#     with open((Path(__file__).parent / "input.txt")) as f:
-#         passwords = [PasswordPolicy.from_text_line(line) for line in f.readlines()]
-#     print(valid_password_counter(passwords))
+def test_treemap_access():
+    treemap = [TreeRow("..##."), TreeRow("#...#")]
+    assert treemap[1][9] == "#"
+
+
+def test_treemap_slope_tree_count():
+    treemap = [
+        TreeRow("..##......."),
+        TreeRow("#...#...#.."),
+        TreeRow(".#....#..#."),
+        TreeRow("..#.#...#.#"),
+        TreeRow(".#...##..#."),
+        TreeRow("..#.##....."),
+        TreeRow(".#.#.#....#"),
+        TreeRow(".#........#"),
+        TreeRow("#.##...#..."),
+        TreeRow("#...##....#"),
+        TreeRow(".#..#...#.#"),
+    ]
+    assert slope_tree_count(treemap) == 7
+
+
+if __name__ == "__main__":
+    with open((Path(__file__).parent / "input.txt")) as f:
+        treemap = [TreeRow(line.rstrip("\n")) for line in f]
+    print(slope_tree_count(treemap))
